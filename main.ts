18bf9a185ff89b24da3186b4ab3c5968e8199d61
lmezmor752 <132547384+lmezmor752@users.noreply.github.com>
2024-05-20 09:47:42 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(4)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.setGroup(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(2)
+})
+radio.setGroup(95)
+basic.forever(function () {
+	
+})
